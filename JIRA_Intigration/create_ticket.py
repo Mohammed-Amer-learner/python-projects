1f57b756612b2277f9213631c5ccde427ef8c7cd
Mohammed Amer <155807217+Mohammed-Amer-learner@users.noreply.github.com>
2025-06-19 12:17:27 +0530

    Create create_ticket.py

---
+++ JIRA_Intigration/create_ticket.py
@@ -0,0 +1,51 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+url = "https://mohammedamer.atlassian.net/rest/api/3/issue"
+
+API_TOKEN = "Enter Api key here"
+
+auth = HTTPBasicAuth("mohammedamer9553@gmail.com", API_TOKEN)
+
+headers = {
+  "Accept": "application/json",
+  "Content-Type": "application/json"
+}
+
+payload = json.dumps( {
+  "fields": {
+    "description": {
+      "content": [
+        {
+          "content": [
+            {
+              "text": "My first JIRA project",
+              "type": "text"
+            }
+          ],
+          "type": "paragraph"
+        }
+      ],
+      "type": "doc",
+      "version": 1
+    },
+    "issuetype": {
+      "id": "10001"
+    },
+    "project": {
+      "key": "MFP"
+    },
+  },
+  "update": {}
+} )
+
+response = requests.request(
+   "POST",
+   url,
+   data=payload,
+   headers=headers,
+   auth=auth
+)
+
+print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
